7bf4322a9f6d05302877ea1d7c748c1fa9a01e09
Manuel Erdös <manuel.erdoes@bluewin.ch>
2024-10-06 09:47:53 +0500

    version

--- src/lib/version.js
+++ src/lib/version.js
@@ -4,6 +4,6 @@
 // 1.0.2  1.10.2024    M. Erdös    Responsive Design
 // 1.0.1  1.10.2024    M. Erdös    Bugfix in Search.jsx -> now text search doesnt find not-public blogs anymore 
 
-export const appVersion = "1.0.4";
+export const appVersion = "1.0.5";
 
 
